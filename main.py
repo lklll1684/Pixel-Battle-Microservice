9f43bace2f7f011b2f8ebcfbf3a7845c76a59cb6
lklll1684 <lyubakolpakova2003@gmail.com>
2026-08-31 19:18:17 +0300

    feat: integrate clickhouse for pixel change history

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ import time
 import asyncio
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 import redis.asyncio as redis
+import clickhouse_connect
 from pyexpat.errors import messages
 
 app = FastAPI()
 
 r = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
-
+ch_client = clickhouse_connect.get_client(host='localhost', port=8123, password='1234')
 WIDTH = 100
 HEIGHT = 100
 COOLDOWN = 30
@@ -63,6 +64,7 @@ async def websocket_endpoint(websocket: WebSocket):
             await r.set(f"user:{user_id}:cd", "1", ex=COOLDOWN)
             pixel_index = y * WIDTH + x
             await r.hset("canvas", pixel_index, color)
+            ch_client.insert('pixel_history', [[x, y, color, user_id]], column_names=['x', 'y', 'color', 'user_id'])
             event = json.dumps({"x": x, "y": y, "color": color, "user": user_id})
             await r.publish("pixel_updates", event)
     except WebSocketDisconnect:
